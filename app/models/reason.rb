7d26f7aa296a967377a597733c6947ba5f4ee1f7
Bret <breton.burnett@gmail.com>
2025-02-19 17:04:41 -0600

    Update docker-compose.dev to fix local environment and migrate. Create ReadMe content. Add enum to reason type attribute.

--- app/models/reason.rb
+++ app/models/reason.rb
@@ -1,4 +1,6 @@
 class Reason < ApplicationRecord
+  enum type: { transaction_data: "transaction_data", rug_pull: "rug_pull", token_supply: "token_supply", social_media_presence: "social_media_presence", black_list: "black_list", summary: "summary" }
+
   belongs_to :suggestion
   has_many :references, dependent: :destroy
 
