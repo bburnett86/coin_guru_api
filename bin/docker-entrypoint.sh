91667ae63944b1ff017920ff65c3e95781466193
Bret <breton.burnett@gmail.com>
2025-02-26 12:34:53 -0600

    Changes for Docker

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -6,5 +6,10 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile)
+# Wait for the database to be ready
+until pg_isready -h "$POSTGRES_HOST" -U "$POSTGRES_USER"; do
+  echo "Waiting for database..."
+  sleep 2
+done
+
 exec "$@"
